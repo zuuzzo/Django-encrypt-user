a484af00b68e1f6a462d9394fd1dd0e48e912e9b
XsTayLXneKit <azabitiy@gmail.com>
2023-04-05 20:27:05 -0400

    2nd

---
+++ mysite/myapp/static/js/script.js
@@ -0,0 +1,11 @@
+// Open login form
+function openLoginForm() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("login-form").style.display = "block";
+}
+
+// Close login form
+function closeLoginForm() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("login-form").style.display = "none";
+}
